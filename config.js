ed1353cf7daa82c85d7101aa0531ee227dc44592
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2024-02-06 22:30:26 +0530

    updates

--- config.js
+++ config.js
@@ -3,25 +3,31 @@ var environments = {};
 
 //staging (default) environment
 environments.staging = {
-	'httpPort':3000,
-	'httpsPort':3001,
-	'envName': 'staging',
-	'hashingSecret' : 'this is a secret'
+	httpPort: 30002,
+	httpsPort: 3001,
+	envName: "staging",
+	hashingSecret: "this is a secret",
 };
 
 // production environment
 environments.production = {
-	'httpPort':5000,
-	'httpsPort':5001,
-	'envName':'production',
-	'hashingSecret' : 'this is a secret'
+	httpPort: 5000,
+	httpsPort: 5001,
+	envName: "production",
+	hashingSecret: "this is a secret",
 };
 
 // determine which environment was passed as command line argument
-var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLocaleLowerCase(): 'staging';
+var currentEnvironment =
+	typeof process.env.NODE_ENV == "string"
+		? process.env.NODE_ENV.toLocaleLowerCase()
+		: "staging";
 
 // check if the environment name passed is a valid env name
-var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+var environmentToExport =
+	typeof environments[currentEnvironment] == "object"
+		? environments[currentEnvironment]
+		: environments.staging;
 
 // export the module
 module.exports = environmentToExport;
